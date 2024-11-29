f00676959feeec00cabaa00852fd0d5594446771
Andrés Correa <andres.correar@est.iudigital.edu.co>
2024-11-29 11:52:17 -0500

    Terminado

--- FuncionPermuta.js
+++ FuncionPermuta.js
@@ -1,12 +1,12 @@
 function funcionPrincipal() {
     let a = 10;
-    let b = 5;
+    let b = 8;
     let resultado = ""; 
 
     
     function permuta() {
-        resultado += "El valor inicial de a es: " + a + "<br>";
-        resultado += "El valor inicial de b es: " + b + "<br>";
+        resultado += "El valor de a es: " + a + "<br>";
+        resultado += "El valor de b es: " + b + "<br>";
     
         let NumeroTemporal = a;
         a = b;
